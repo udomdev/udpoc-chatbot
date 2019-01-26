aedea461c13f65f08b1c100f4c35da075c4988b7
udomdev <udomdev@gmail.com>
2019-01-27 02:12:24 +0700

    try to fix error7

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    app.logger.info("X-Line-Signature:"+ signature)
 
     # handle webhook body
     try:
