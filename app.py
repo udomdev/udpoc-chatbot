98fbb00218c84b3b971e2d22eb026e7c0da3329f
udomdev <udomdev@gmail.com>
2019-01-26 23:58:08 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+
+app = Flask(__name__)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+@app.route('/')
+def hello_world():
+    the_time = datetime.now().strftime("%A, %d-%b-%Y %H:%M:%S")
+    return """ 
+    <h1>POC Line-Bot</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.replay_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+if __name__ == '__main__':
+    app.debug(True)
+    app.run()
