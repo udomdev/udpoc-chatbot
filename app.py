1c08c595e629f90161f1ff3ed2ac90dadb7d9cc6
udomdev <udomdev@gmail.com>
2019-01-27 02:42:08 +0700

    enable logging

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ if channel_access_token is None:
 
 # gunicorn_error_logger = logging.getLogger('gunicorn.error')
 # app.logger.handlers.extend(gunicorn_error_logger.handlers)
-# app.logger.setLevel(logging.DEBUG)
-# app.logger.debug('this will show in the log')
+app.logger.setLevel(logging.DEBUG)
+app.logger.debug('this will show in the log')
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
