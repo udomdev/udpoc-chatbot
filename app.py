82b730446ef2439fd69e5ba3c47aa985894a6437
udomdev <udomdev@gmail.com>
2019-01-27 02:37:59 +0700

    try to fix error12

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN')
     sys.exit(1)
 
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-app.logger.setLevel(logging.DEBUG)
-app.logger.debug('this will show in the log')
+# gunicorn_error_logger = logging.getLogger('gunicorn.error')
+# app.logger.handlers.extend(gunicorn_error_logger.handlers)
+# app.logger.setLevel(logging.DEBUG)
+# app.logger.debug('this will show in the log')
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
